2856a813f61ca84bb1a1b64ab2f06226db5be60c
prishachalak <chalakprisha@gmail.com>
2024-06-15 13:56:22 +0800

    editing

--- server/config.js
+++ server/config.js
@@ -1,5 +1,1 @@
 export const DATABASE = "mongodb+srv://prisha:orbital2024@task-pets.wedt7jj.mongodb.net/?retryWrites=true&w=majority&appName=task-pets";
-// const DATABASE = "mongodb://localhost:27017/dbname";
-//const JWT_SECRET="secrat11223344";
-//SENDGRID_KEY="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";
-//const EMAIL_FROM="yourname@gmail.com";
